7149b8c51425bebfa2f93ffce686ee40add66543
aaguero64 <71055422+aaguero64@users.noreply.github.com>
2020-09-17 11:16:19 -0700

    Add files via upload

--- App.java
+++ App.java
@@ -9,22 +9,22 @@ public class App {
 		double itemPrice = peachPrice + orangePrice;
 		double moneyInWallet = 125.30;
 		int numberOfFriend = 15; // 
-		int myAge = 57;
+		double myAge = 57;
 		String firstName = "Adrian";
 		String lastName = "Aguero";
 		String middleName = "C";
 		
 		
 		double newMoneyInWallet = moneyInWallet - itemPrice;
-		System.out.println(newMoneyInWallet);
+		System.out.println("Money left in wallet:" + " " + "$" + newMoneyInWallet);
 		
 		
 		double friendByYear = numberOfFriend / myAge; 
-		System.out.println(friendByYear);
+		System.out.println("Number of friends by year: " + " " + friendByYear);
 		
 				
 		String fullName = firstName + " " +middleName + " " + lastName;
-		System.out.println(fullName);
+		System.out.println("Full Name: " + fullName);
 		
 		
 		
